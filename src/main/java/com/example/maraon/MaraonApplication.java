c1ea8d83aebbe1e57ad0de38f3ec02e2d5ade191
gun1kim <obkh31@naver.com>
2024-12-10 23:25:26 +0900

    initial commit

---
+++ src/main/java/com/example/maraon/MaraonApplication.java
@@ -0,0 +1,13 @@
+package com.example.maraon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaraonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaraonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maraon/MaraonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maraon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaraonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
